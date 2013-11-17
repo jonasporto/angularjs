daf8c771f7588eeac2edfac6c34d4ddf606ccd05
jonasporto <jporto.contato@hotmail.com>
2013-11-17 16:16:01 -0200

    completo

--- js/custom.js
+++ js/custom.js
@@ -4,23 +4,65 @@ function TesteCtrl($scope,$http,$window){
 
 	$scope.salvar = function(){
 
+		if($scope.unidade.id>0){
+			
+			$.ajax({
+				type: 'put',
+				url: 'service/produtos',
+				data: $scope.unidade,
+				success:function(data){
+					$window.console.log(data);
+				}
+
+			});
+
+
+		}else{
 
 		$http.post('service/produtos',$scope.unidade).success(function(data){
-			$window.console.log(data);
-
+			$scope.lista.unshift(data);
+			reset();
 		});
 
-		/*
-		$http.get('/service/produtos').success(function(data){
-			$windows.console.log
-		});
-		*/
 	}
+	};
+
+	$scope.editar = function(unidade){
+		$scope.unidade = unidade;
+
+
+
+	};
+
+
+
+	$scope.excluir = function(unidade){
+		var confirm = $window.confirm('Tem certeza que deseja excluir o produto' + unidade.title + '?');
+		if(confirm){
+			$http.delete('service/produtos/' + unidade.id).success(function(data){
+				var index = $scope.lista.indexOf(unidade);
+				$scope.lista.splice(index,1);
+
+
+			});
+		}
+	
+
+	};
+
+
+	var reset = function(){
+		$scope.unidade = {id:0,category:'',title:'',google_id:''};
+
+	};
 
 
 	var init = function(){
-		$scope.unidade = {id:0,category:'',title:'',google_id:''};
-		$scope.lista = [];
+		$http.get('service/produtos').success(function(data){
+			$scope.lista = data;
+		});
+			reset();
+						
 	};
 
 	init();
