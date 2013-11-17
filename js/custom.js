34ce4709d16f020781d234df3420d01183f3c75d
jonasporto <jporto.contato@hotmail.com>
2013-11-17 15:24:47 -0200

    ambiente funcionando alterações no DB

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,28 @@
-alert("funfou");
+function TesteCtrl($scope,$http,$window){
+
+	var $ = jQuery;
+
+	$scope.salvar = function(){
+
+
+		$http.post('service/produtos',$scope.unidade).success(function(data){
+			$window.console.log(data);
+
+		});
+
+		/*
+		$http.get('/service/produtos').success(function(data){
+			$windows.console.log
+		});
+		*/
+	}
+
+
+	var init = function(){
+		$scope.unidade = {id:0,category:'',title:'',google_id:''};
+		$scope.lista = [];
+	};
+
+	init();
+
+}
